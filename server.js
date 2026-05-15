6059458f6eabb33209b2fed46598499d25baa222
YourActualNameOrAlias <you@email.com>
2026-05-15 16:44:12 +1200

    Fix server.js with proper static file serving

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-PASTE_OF_SERVER_JS_ALREADY_PROVIDED_IN_PREVIOUS_STEP
+﻿const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
